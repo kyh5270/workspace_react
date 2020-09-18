0696e779d6a49afc773e0f7795d34e71b82e09fa
DESKTOP-8QQ1TEE\User <kyh5270@gmail.com>
2020-09-18 21:48:25 +0900

    Updates

---
+++ precache-manifest.54b8e8732a143027cc2ed9ac71463fa1.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "339cc06c5623cf074677d5c224981413",
+    "url": "/workspace_react/index.html"
+  },
+  {
+    "revision": "67108d7bb52298b1b321",
+    "url": "/workspace_react/static/css/main.f55dcced.chunk.css"
+  },
+  {
+    "revision": "b733a9f86170dc1e85b0",
+    "url": "/workspace_react/static/js/2.1ed3689d.chunk.js"
+  },
+  {
+    "revision": "67108d7bb52298b1b321",
+    "url": "/workspace_react/static/js/main.6f06c8ec.chunk.js"
+  },
+  {
+    "revision": "a0a164a7633d64d9448c",
+    "url": "/workspace_react/static/js/runtime-main.f6b8860c.js"
+  }
+]);
